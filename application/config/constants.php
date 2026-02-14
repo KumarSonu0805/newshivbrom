fdabdd63a076ca11d0e8da8e504759d6fc990dfd
Atal Prateek Barla <prateek.atal@gmail.com>
2026-02-14 10:55:33 +0530

    s

--- application/config/constants.php
+++ application/config/constants.php
@@ -123,7 +123,7 @@ if(isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST']=='localhost')){
 }
 else{
 	defined('DB_HOST')      ? null : define('DB_HOST', '127.0.0.1');
-	defined('DB_USER')      ? null : define('DB_USER', '');
-	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'Shivbrom@123#$');
-	defined('DB_NAME')      ? null : define('DB_NAME', '');
+	defined('DB_USER')      ? null : define('DB_USER', 'u711511560_user_newshivb');
+	defined('DB_PASSWORD')  ? null : define('DB_PASSWORD', 'NewShivBrom@123#$');
+	defined('DB_NAME')      ? null : define('DB_NAME', 'u711511560_db_newshivb');
 }
