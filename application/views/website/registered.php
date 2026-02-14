b2866a18923c0c45d0fd910c1374bb787c9afc8c
Atal Prateek Barla <prateek.atal@gmail.com>
2026-02-14 15:08:45 +0530

    s

--- application/views/website/registered.php
+++ application/views/website/registered.php
@@ -10,7 +10,7 @@
                         <div class="row">
                             <div class="col-md-1"></div>
                             <div class="col-md-10">
-                                <h4>Welcome <?php echo $this->session->flashdata('mname'); ?>,</h4><br>
+                                <h5>Welcome <?php echo $this->session->flashdata('mname'); ?>,</h5><br>
                                 <ul style="list-style:none;">
                                     <li><h4>Username : <?php echo $this->session->flashdata('uname');?></h4><br></li>
                                     <li><h4>Password : <?php echo $this->session->flashdata('pass');?></h4></li>
